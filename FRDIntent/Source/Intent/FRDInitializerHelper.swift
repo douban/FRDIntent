dc7aa72ec243ebf318dddd1c9ff79e12df1b784a
木瓜 <lforme@163.com>
2018-11-28 17:40:47 +0800

    add FRDInitializerHelper swift class

---
+++ FRDIntent/Source/Intent/FRDInitializerHelper.swift
@@ -0,0 +1,19 @@
+//
+//  FRDInitializerHelper.swift
+//  FRDIntent
+//
+//  Created by mugua on 2018/11/28.
+//  Copyright © 2018 Douban Inc. All rights reserved.
+//
+
+import UIKit
+
+class FRDInitializerHelper {
+
+    static func viewControllerFromClazz(_ clazz: FRDIntentReceivable.Type?, extras: [String: Any]) -> FRDIntentReceivable? {
+        
+        guard let controllerClass = clazz else { return nil }
+        let vc = controllerClass.init(extras: extras)
+        return vc
+    }
+}
